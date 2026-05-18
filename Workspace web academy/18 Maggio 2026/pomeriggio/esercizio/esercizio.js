4321f3120a43533df143b9d59575d328410bf5cf
Marienne-ma <154588375+Marienne-ma@users.noreply.github.com>
2026-05-18 16:43:33 +0200

    aggiunto esercizio 1 extra del pomeriggio

--- Workspace web academy/18 Maggio 2026/pomeriggio/esercizio/esercizio.js
+++ Workspace web academy/18 Maggio 2026/pomeriggio/esercizio/esercizio.js
@@ -1,57 +1,88 @@
 const prompt = require("prompt-sync")();
-let request;
-let numeri = [];
-let numeri_pari = [];
-let numeri_dispari = [];
-let somma = 0;
-let n_max = 0;
-let n_min = 0;
+// let request;
+// let numeri = [];
+// let numeri_pari = [];
+// let numeri_dispari = [];
+// let somma = 0;
+// let n_max = 0;
+// let n_min = 0;
 
+
+// do {
+//     request = prompt("Inserisci un numero intero, peer finire inserisci 0:\n");
+
+//     request = Number(request);
+
+//     if (!Number.isNaN(request) && request > 0)
+//         numeri.push(request);
+
+// } while (request !== 0);
+
+// for (let n of numeri) {
+
+//     n % 2 === 0 ? numeri_pari.push(n) : numeri_dispari.push(n);
+
+//     somma += n;
+
+// }
+
+// if(numeri.length !== 0)
+// {
+// n_max = Math.max(...numeri);
+// n_min = Math.min(...numeri);
+// } else {
+//     numeri = "nessun numero inserito";
+// }
+
+
+
+// if(numeri_pari.length === 0) numeri_pari = "nessun numero pari inserito";
+
+// if(numeri_dispari.length === 0) numeri_dispari = "nessun numero dispari inserito";
+
+
+
+// console.log("numeri inseriti: \n" +
+//     numeri +
+//     "\nnumeri pari: \n" +
+//     numeri_pari +
+//     "\nnumeri dispari: \n" +
+//     numeri_dispari +
+//     "\nla somma: \n" +
+//     somma +
+//     "\nnumero più grande: \n" +
+//     n_max +
+//     "\nnumero più piccolo: \n" +
+//     n_min
+// );
+
+
+
+
+// EXTRA Provare a gestire un array di array dove carichi nell array a posizione 0 le stringhe e in array a posizione 1 i numeri
+
+
+let vario = [[], []];
 
 do {
-    request = prompt("Inserisci un numero intero, peer finire inserisci 0:\n");
+    request = prompt("Inserisci un valore, stringa o numero, per finire inserisci 'esci':\n");
 
-    request = Number(request);
 
-    if (!Number.isNaN(request) && request > 0)
-        numeri.push(request);
+    if (request !== "esci") {
 
-} while (request !== 0);
+       let x = Number(request);
 
-for (let n of numeri) {
+        Number.isNaN(x) ? vario[0].push(request) : vario[1].push(x);
+    }
 
-    n % 2 === 0 ? numeri_pari.push(n) : numeri_dispari.push(n);
 
-    somma += n;
+} while (request !== "esci");
 
-}
 
-if(numeri.length !== 0)
-{
-n_max = Math.max(...numeri);
-n_min = Math.min(...numeri);
-} else {
-    numeri = "nessun numero inserito";
-}
+console.log("stringhe inserite: \n"+vario[0]+
+    "\n numeri inseriti: \n"+vario[1]
+)
 
 
 
-if(numeri_pari.length === 0) numeri_pari = "nessun numero pari inserito";
 
-if(numeri_dispari.length === 0) numeri_dispari = "nessun numero dispari inserito";
-
-
-
-console.log("numeri inseriti: \n" +
-    numeri +
-    "\nnumeri pari: \n" +
-    numeri_pari +
-    "\nnumeri dispari: \n" +
-    numeri_dispari +
-    "\nla somma: \n" +
-    somma +
-    "\nnumero più grande: \n" +
-    n_max +
-    "\nnumero più piccolo: \n" +
-    n_min
-);
