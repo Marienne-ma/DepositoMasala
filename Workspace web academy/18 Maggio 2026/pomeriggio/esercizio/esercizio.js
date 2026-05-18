0563fa693af4f8156ce8503b56e3518c18acd768
Marienne-ma <154588375+Marienne-ma@users.noreply.github.com>
2026-05-18 16:15:30 +0200

    esercizio 1 pomeriggio 18 maggio 2026

---
+++ Workspace web academy/18 Maggio 2026/pomeriggio/esercizio/esercizio.js
@@ -0,0 +1,48 @@
+const prompt = require("prompt-sync")();
+let request;
+let numeri = [];
+let numeri_pari = [];
+let numeri_dispari = [];
+let somma = 0;
+let n_max = 0;
+let n_min = 0;
+
+
+do {
+    request = prompt("Inserisci un numero intero, peer finire inserisci 0:\n");
+
+    request = Number(request);
+
+    if (!Number.isNaN(request) && request > 0)
+        numeri.push(request);
+
+} while (request !== 0);
+
+for (let n of numeri) {
+
+    n % 2 === 0 ? numeri_pari.push(n) : numeri_dispari.push(n);
+
+    somma += n;
+
+}
+
+n_max = Math.max(...numeri);
+n_min = Math.min(...numeri);
+
+if(numeri_pari.length === 0) numeri_pari = "nessun numero pari inserito";
+
+if(numeri_dispari.length === 0) numeri_dispari = "nessun numero dispari inserito";
+
+console.log("numeri inseriti: \n" +
+    numeri +
+    "\nnumeri pari: \n" +
+    numeri_pari +
+    "\nnumeri dispari: \n" +
+    numeri_dispari +
+    "\nla somma: \n" +
+    somma +
+    "\nnumero più grande: \n" +
+    n_max +
+    "\nnumero più piccolo: \n" +
+    n_min
+);
---
+++ Workspace web academy/18 Maggio 2026/pomeriggio/prova.js
@@ -0,0 +1,65 @@
+// let numeri = [1, 2, 3];
+
+
+// // Accesso agli elementi
+
+// console.log(numeri[0]); // 1
+
+
+// // Aggiunta elementi
+
+// numeri.push(4);        // aggiunge in fondo -> [1,2,3,4]
+
+// numeri.unshift(0);     // aggiunge all'inizio -> [0,1,2,3,4]
+
+
+// // Rimozione elementi
+
+// numeri.pop();          // rimuove ultimo -> [0,1,2,3]
+
+// numeri.shift();        // rimuove primo -> [1,2,3]
+
+
+// // Iterazione
+
+// numeri.forEach(n => console.log(n));
+
+
+// // Trasformazione
+
+// let doppi = numeri.map(n => n * 2); // [2,4,6]
+
+
+// // Filtro
+
+// let maggioriDiUno = numeri.filter(n => n > 1); // [2,3]
+
+
+// // Ricerca
+
+// let trovato = numeri.find(n => n === 2); // 2
+
+
+// // Lunghezza
+
+// console.log(numeri.length); // 3
+
+
+let numeri = ["10", "20", "30"];
+
+
+for (let valore of numeri) {
+
+    console.log(valore); // 10, 20, 30
+
+}
+
+
+// let persona = { nome: "Mario", eta: 30 };
+
+
+// for (let chiave in persona) {
+
+//     console.log(chiave, persona[chiave]);
+
+// }
