ec22ddefe9cd2adc728386b3c16bbb0e14e6be28
Marienne-ma <154588375+Marienne-ma@users.noreply.github.com>
2026-05-18 14:37:45 +0200

    correzione esercizio 2 javascript 18 maggio 2026

---
+++ 18 Maggio 2026/mattina/esercizio/esercizioCicli.js
@@ -0,0 +1,47 @@
+const prompt = require("prompt-sync")();
+let condition;
+let numeri_pari = 0;
+let numeri_dispari = 0;
+
+do {
+
+    let request = prompt("Inserisci un numero intero positivo:\n");
+
+
+        request = Number(request);
+
+        if (Number.isNaN(request) || request < 0) {
+
+            console.log("valore non valido");
+            condition = false;
+
+        } else {
+
+            condition = true;
+
+        }
+
+
+    if (condition) {
+
+        for (let i = 0; i <= request; i++) {
+
+            if (i % 2 === 0) {
+                numeri_pari++;
+            } else {
+                numeri_dispari++;
+            }
+
+        }
+
+        console.log("numeri pari compreso lo 0: " + numeri_pari + ", numeri dispari : " + numeri_dispari);
+      
+
+    } 
+
+
+} while (!condition);
+
+
+
+
