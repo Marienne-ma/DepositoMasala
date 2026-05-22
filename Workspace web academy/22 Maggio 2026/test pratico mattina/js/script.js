59679146ce8d868c8c390d6b463918c70dd16f71
Marienne-ma <154588375+Marienne-ma@users.noreply.github.com>
2026-05-22 12:40:58 +0200

    modifiche navbar test pratico 22 maggio

--- Workspace web academy/22 Maggio 2026/test pratico mattina/js/script.js
+++ Workspace web academy/22 Maggio 2026/test pratico mattina/js/script.js
@@ -5,12 +5,9 @@
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("nav-links");
 
-if (hamburger && navLinks) {
-  hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
-}
-
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
 
 // =========================
 // FORM CONTATTI
